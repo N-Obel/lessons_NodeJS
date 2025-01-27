8fa28581d03e9eb167c29112b73bfa4ec6ed61e1
Nikita <123548456+N-Obel@users.noreply.github.com>
2025-01-27 04:38:05 +0200

    lesson_11 (auth, jwt_token, front, back)

--- lesson_11/frontend/scripts/RequestManager.js
+++ lesson_11/frontend/scripts/RequestManager.js
@@ -1,6 +1,7 @@
 class RequestManager {
   // Базовий URL для API запитів
-  static apiBase = 'http://localhost:3000/api/v1'
+  // static apiBase = 'http://localhost:3000/api/v1'
+  static apiBase = 'https://lessons-11.onrender.com/api/v1'
 
   // Метод для отримання повного маршруту до сервера
   static getServerRoute(path) {
